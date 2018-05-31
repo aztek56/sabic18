9bf81b201a37d0c7bfdd05f8da546a28f4c8b8d6
Gabriel Castillo <gcastillo@ucdavis.edu>
2018-05-30 18:02:56 -0700

    Modified the form to have methods for submission

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,33 @@
-import React, {Component} from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
 
 class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            email: '',
+            password: '',
+            errors: ''
+        };
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange(event) {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+
+        const newUser = {
+            email: this.state.email,
+            password: this.state.password,
+        };
+
+        console.log(newUser);
+    }
+
     render() {
         return (
             <div className="login">
@@ -9,19 +35,25 @@ class Login extends Component {
                     <div className="row">
                         <div className="col-md-8 m-auto">
                             <h1 className="display-4 text-center">Log In</h1>
-                            <p className="lead text-center">Sign in to your My Social Network account</p>
-                            <form action="dashboard.html">
+                            <p className="lead text-center">Sign in to your DevConnector account</p>
+                            <form onSubmit={this.onSubmit}>
                                 <div className="form-group">
                                     <input type="email"
                                            className="form-control form-control-lg"
                                            placeholder="Email Address"
-                                           name="email"/>
+                                           name="email"
+                                           value={this.state.email}
+                                           onChange={this.onChange}
+                                    />
                                 </div>
                                 <div className="form-group">
                                     <input type="password"
                                            className="form-control form-control-lg"
                                            placeholder="Password"
-                                           name="password"/>
+                                           name="password"
+                                           value={this.state.password}
+                                           onChange={this.onChange}
+                                    />
                                 </div>
                                 <input type="submit" className="btn btn-info btn-block mt-4"/>
                             </form>
